68556d18e7ea77287f981390ee637a25326e383e
Murphy <bestyoooo@163.com>
2023-09-04 08:28:53 +0800

    springboot-3 练习测试

---
+++ springboot-jedis-5/src/main/java/com/atguigu/redis/config/AppRedisConfiguration.java
@@ -0,0 +1,21 @@
+package com.atguigu.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class AppRedisConfiguration {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        //把对象转为json字符串的序列化工具
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        System.out.println("template = " + template);
+        return template;
+    }
+}
---
+++ springboot-jedis-5/src/test/java/com/atguigu/redis/test/RedisTest.java
@@ -0,0 +1,27 @@
+package com.atguigu.redis.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.List;
+
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    public void test(){
+        redisTemplate.opsForValue().set("哈克", "哈克学习是真卷啊");
+        Object hake = redisTemplate.opsForValue().get("哈克");
+        System.out.println("hake = " + hake);
+
+        redisTemplate.opsForList().leftPush("0615","阿细" );
+        redisTemplate.opsForList().leftPush("0615","哈克" );
+        redisTemplate.opsForList().leftPush("0615","阿毅" );
+        List range = redisTemplate.opsForList().range("0615", 0, -1);
+        range.forEach(System.out::println);
+
+    }
+}
